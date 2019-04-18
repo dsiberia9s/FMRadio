382bf90ba156a1f7e5f83925cd9918be5d53269e
Dimi <dsiberia9s@protonmail.com>
2019-04-18 21:22:53 +0700

    Update main.py

--- main.py
+++ main.py
@@ -49,6 +49,7 @@ RDA5807M_REG_CONFIG = 0x02
 RDA5807M_REG_TUNING = 0x03
 RDA5807M_REG_VOLUME = 0x05
 RDA5807M_REG_BLEND = 0x07
+RDA5807M_REG_STATUS = 0x0A
 RDA5807M_FLG_ENABLE = 0x0001
 RDA5807M_FLG_SEEKUP = 0x0200
 RDA5807M_FLG_SEEK = 0x0100
@@ -62,6 +63,7 @@ RDA5807M_FLG_DMUTE = 0x4000
 RDA5807M_STATUS_STC = 0x4000
 RDA5807M_VOLUME_MASK = 0x000F
 RDA5807M_SPACE_MASK = 0x0003
+RDA5807M_READCHAN_MASK = 0x03FF
 MUTE = False
 
 #lcd.clear()
@@ -101,17 +103,15 @@ def volumeUp():
 def getBandAndSpacing():
   band = read_u16(RDA5807M_REG_TUNING) & (RDA5807M_BAND_MASK | RDA5807M_SPACE_MASK)
   space = band & RDA5807M_SPACE_MASK
-  lcd.clear()
-  lcd.print(space, 10, 10, 0xffffff)
   if (band & RDA5807M_BAND_MASK == RDA5807M_BAND_EAST) and not (read_u16(RDA5807M_REG_BLEND) & RDA5807M_FLG_EASTBAND65M):
     band = (band >> RDA5807M_BAND_SHIFT) + 1
   else:
-    band >>= RDA5807M_BAND_SHIFT
+    band = band >> RDA5807M_BAND_SHIFT
   return space, band
   
 def getFrequency():
-  spaceandbandI, spaceandbandII  = getBandAndSpacing()
-  return RDA5807M_BandLowerLimits[lowByte(spaceandbandI)] + (read_u16(RDA5807M_REG_STATUS) & RDA5807M_READCHAN_MASK) * RDA5807M_ChannelSpacings[highByte(spaceandbandII)] / 10
+  space, band = getBandAndSpacing()
+  return RDA5807M_BandLowerLimits[lowByte(space)] + (read_u16(RDA5807M_REG_STATUS) & RDA5807M_READCHAN_MASK) * RDA5807M_ChannelSpacings[highByte(band)] / 10
 
 
 write_u16(RDA5807M_REG_CONFIG, (RDA5807M_FLG_DHIZ | RDA5807M_STATUS_STC | RDA5807P_FLG_I2SSLAVE | RDA5807M_FLG_SEEKUP | RDA5807M_FLG_RDS | RDA5807M_FLG_NEW | RDA5807M_FLG_ENABLE))
@@ -124,7 +124,9 @@ def buttonB_pressed():
   seekUp()
 
 def buttonC_pressed():
-  getBandAndSpacing()
+  lcd.clear()
+  lcd.print(getFrequency(), 10, 10, 0xffffff)
+  
   #lcd.clear()
   #lcd.print(getFrequency(), 0, 0, 0xffffff)
 
